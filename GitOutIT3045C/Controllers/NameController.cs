67a8e695fb7b2c404c9318c5cfbcbde1b0377226
whsiq <staggsce@mail.uc.edu>
2023-03-23 16:49:14 -0400

    Created NameController

---
+++ GitOutIT3045C/Controllers/NameController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitOutIT3045C.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var name = "Chase Staggs";
+            return Ok(new { Name = name });
+        }
+    }
+}
