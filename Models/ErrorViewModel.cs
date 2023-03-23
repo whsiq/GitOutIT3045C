a00fbfc8a82790ebd235e92dd3afb019b0e1d587
whsiq <staggsce@mail.uc.edu>
2023-03-23 16:31:30 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GitOutIT3045C.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
